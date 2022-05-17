350a16fb3799cf7bb49673c1f90ac6d3fa28cfeb
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-05-17 11:00:18 +0300

    init routing

--- mpi-frontend/src/app/app.module.ts
+++ mpi-frontend/src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { LoginComponent } from './login/login.component';
+import { NotFoungPageComponent } from './not-foung-page/not-foung-page.component';
+import { MapComponent } from './map/map.component'
+import { RouterModule, Routes } from '@angular/router'
+import { HeaderComponent } from './header/header.component'
+
+const appRoutes: Routes = [
+  {path: '', component: MapComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: '/'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoungPageComponent,
+    MapComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ mpi-frontend/src/app/not-foung-page/not-foung-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotFoungPageComponent } from './not-foung-page.component';
+
+describe('NotFoungPageComponent', () => {
+  let component: NotFoungPageComponent;
+  let fixture: ComponentFixture<NotFoungPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NotFoungPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotFoungPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
