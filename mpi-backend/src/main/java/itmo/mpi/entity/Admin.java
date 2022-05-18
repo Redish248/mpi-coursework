ef99ad412092bb1f5eab913f346700113bbe131b
meier-th <meier.d.th@gmail.com>
2022-05-18 19:06:48 +0300

    [REQUESTS] Take into account dates and availability of ships and crews

--- mpi-backend/src/main/java/itmo/mpi/entity/Admin.java
+++ mpi-backend/src/main/java/itmo/mpi/entity/Admin.java
@@ -1,6 +1,6 @@
 package itmo.mpi.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -27,7 +27,7 @@ public class Admin {
     private String nick;
 
     @Column(name = "password", nullable = false, length = 100)
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
 }
