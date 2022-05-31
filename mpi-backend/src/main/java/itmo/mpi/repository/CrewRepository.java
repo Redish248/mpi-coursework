9304346e6a9d4c747b43361c726de68ffaf10658
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-05-31 12:59:46 +0300

    return point

--- mpi-backend/src/main/java/itmo/mpi/repository/CrewRepository.java
+++ mpi-backend/src/main/java/itmo/mpi/repository/CrewRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Repository
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 
-    @Query(value = "select cr.* from crew cr left join trip_request tr on cruid = tr.crew_id and tr.date_start <= " +
+    @Query(value = "select cr.* from crew cr left join trip_request tr on cr.uid = tr.crew_id and tr.date_start <= " +
             ":dateEnd and tr.date_end >= :dateStart AND tr.status IN ('COMPLETE', 'APPROVED_BY_CREW', 'APPROVED_BY_BOTH') where tr.uid is null;", nativeQuery = true)
     List<Crew> getFreeCrewsForDates(@Param("dateStart") LocalDate startDate, @Param("dateEnd") LocalDate endDate);
 
