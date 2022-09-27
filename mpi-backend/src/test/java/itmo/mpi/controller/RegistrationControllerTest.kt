58fd83c5a286fde905e92b2edb37afacf9020f2a
Irina Redkina <irisha132132@mail.ru>
2022-09-28 00:05:58 +0300

    check mockk annotations

--- mpi-backend/src/test/java/itmo/mpi/controller/RegistrationControllerTest.kt
+++ mpi-backend/src/test/java/itmo/mpi/controller/RegistrationControllerTest.kt
@@ -1,4 +1,47 @@
 package itmo.mpi.controller
 
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import itmo.mpi.entity.User
+import itmo.mpi.service.AdminService
+import itmo.mpi.service.UserService
+import itmo.mpi.utils.CommonUtils
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
 class RegistrationControllerTest {
+
+    @MockK
+    lateinit var userService: UserService
+
+    @MockK
+    lateinit var adminService: AdminService
+
+    @MockK
+    lateinit var commonUtils: CommonUtils
+
+    @InjectMockKs
+    var controller = RegistrationController(userService, adminService, commonUtils)
+
+    @Before
+    fun setUp() {
+        every { userService.createUser(any()) } returns mockedUser()
+    }
+
+    @Test
+    fun testRegistration() {
+
+    }
+
+    private fun mockedUser(): User {
+        val user = User();
+        user.id = 1;
+        user.email = "tst@ya.ru"
+        user.name = "ira"
+        return user;
+    }
 }
