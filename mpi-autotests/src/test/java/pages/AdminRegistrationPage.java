9f607a41cea87e942ad309ba289753722b3d2f69
Irina Redkina <irisha132132@mail.ru>
2022-10-11 20:36:21 +0300

    add admin page test

--- mpi-autotests/src/test/java/pages/AdminRegistrationPage.java
+++ mpi-autotests/src/test/java/pages/AdminRegistrationPage.java
@@ -1,9 +1,36 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class AdminRegistrationPage extends PageObject {
     public AdminRegistrationPage(WebDriver driver) {
         super(driver);
+    }
+
+    private final String USER_TABLE = "//app-user-approval/div/clr-datagrid";
+    private final String USER_TABLE_ACTIVATE = "//*[@id=\"btn-activate\"]";
+    private final String USER_TABLE_DELETE = "//*[@id=\"btn-delete\"]";
+
+    @FindBy(xpath = USER_TABLE)
+    private WebElement userTable;
+
+    @FindBy(xpath = USER_TABLE_ACTIVATE)
+    private WebElement userTableActivate;
+
+    @FindBy(xpath = USER_TABLE_DELETE)
+    private WebElement userTableDelete;
+
+    public boolean isUserTableVisible() {
+        return userTable.isDisplayed();
+    }
+
+    public boolean isActivateButtonVisible() {
+        return userTableActivate.isDisplayed();
+    }
+
+    public boolean isDeleteButtonVisible() {
+        return userTableDelete.isDisplayed();
     }
 }
--- mpi-autotests/src/test/java/tests/AdminRegistartionPageTest.java
+++ mpi-autotests/src/test/java/tests/AdminRegistartionPageTest.java
@@ -1,4 +1,53 @@
 package tests;
 
+import config.WebDriverConfiguration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.AdminRegistrationPage;
+import pages.LogInPage;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AdminRegistartionPageTest {
+    private WebDriver webDriver;
+
+    private AdminRegistrationPage adminRegistrationPage;
+
+    @BeforeEach
+    public void setUp() {
+        webDriver = WebDriverConfiguration.getWebDriver(WebDriverConfiguration.Browser.CHROME);
+        LogInPage logInPage = new LogInPage(webDriver);
+        adminRegistrationPage = new AdminRegistrationPage(webDriver);
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        logInPage.signIn("redish", "12345");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-map")));
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.navigate().to("http://localhost:4200/approvals");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"btn-activate\"]")));
+    }
+
+    @Test
+    @DisplayName("Test that admin page for user approvals available")
+    public void testAdminPage() {
+        assertAll(
+                () -> assertTrue(adminRegistrationPage.isUserTableVisible()),
+                () -> assertTrue(adminRegistrationPage.isActivateButtonVisible()),
+                () -> assertTrue(adminRegistrationPage.isDeleteButtonVisible())
+        );
+    }
+
+    @AfterEach
+    public void tearDown(){
+        webDriver.quit();
+    }
 }
